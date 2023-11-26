cfda8e7af03a1b83d372af8ca862b5697be4682d
Saif Azharuddin <saifA.uddin@gmail.com>
2023-11-26 14:18:14 -0700

    added models dir and campground model + basic new campgr GET

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
 const path = require('path')
+const Campground = require('./models/campground')
 
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// HOME PAGE
 app.get('/', (req, res) => {
     res.render('home')
 })
+
+// NEW CAMPGROUND
+app.get('/makecampground', async (req, res)=> {
+    const camp = new Campground({title: 'My Backyard'});
+    await camp.save();
+    res.send(camp)
+})
+
 app.listen(3000, ()=> {
-    console.log('LISTENING ON PORT 3000')
+    console.log('LISTENING ON PORT 3000 SAH!!!')
 })
