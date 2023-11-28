8fb34efe5560b0ad3c56c924d17de7052a9a17fc
Saif Uddin <saifA.uddin@gmail.com>
2023-11-28 13:02:40 -0700

    made index.js file for seeds, copied over mongoose and express boilerplate

---
+++ seeds/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const Campground = require('./models/campground');
+
+const app = express();
+const path = require('path');
+
+// USE 127.0.0.1 INSTEAD OF LOCALHOST
+mongoose.connect('mongodb://127.0.0.1:27017/yelpCampDb', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("Error, MONGO CONNECTION!!!!")
+        console.log(err)
+    })
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", ()=> {
+    console.log("DB Connected");
+});
