3ea79b663499d0f2e49490835c47464062e688f0
Saif Uddin <saifA.uddiN@gmail.com>
2024-02-16 15:39:28 -0700

    set up ejs-mate, using ejs boilerplate html template now

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const path = require('path');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 
 const Campground = require('./models/campground');
@@ -23,6 +24,7 @@ db.once("open", ()=> {
     console.log("DB Connected");
 })
 
+app.engine('ejs', ejsMate);
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
 
