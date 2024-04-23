f5ad94d091d37d24f2e588a9cdc89673e1369851
Saif Uddin <saifA.uddiN@gmail.com>
2024-04-23 11:37:33 -0700

    added basic error handler to app.js and updated more error logic for EDIT

--- app.js
+++ app.js
@@ -20,16 +20,16 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelpCampDb', { useNewUrlParser: true
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
-db.once("open", ()=> {
+db.once("open", () => {
     console.log("DB Connected");
 })
 
 app.engine('ejs', ejsMate);
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // allows express to parse request bodies into values
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 // use method override
 app.use(methodOverride('_method'));
 
@@ -39,52 +39,62 @@ app.get('/', (req, res) => {
 })
 
 // GET CAMPGROUNDS
-app.get('/campgrounds', async(req, res) => {
+app.get('/campgrounds', async (req, res) => {
     const campgrounds = await (Campground.find({}));
-    res.render('campgrounds/index', {campgrounds})
+    res.render('campgrounds/index', { campgrounds })
 })
 
 // NEW CAMPGROUND PAGE
 // needs to be ABOVE show page else logic will look for campground with ID "new"
-app.get('/campgrounds/new', (req,res)=> {
+app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
 
 // POST for new campground created above
-app.post('/campgrounds' ,async(req, res)=> {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
+app.post('/campgrounds', async (req, res, next) => {
+    try {
+        const campground = new Campground(req.body.campground);
+        await campground.save();
 
-    res.redirect(`/campgrounds/${campground._id}`);
+        res.redirect(`/campgrounds/${campground._id}`);
+    } catch (error) {
+        next(error);
+    }
+
 })
 
 // VIEW SPECIFIC CAMPGROUND DETAILS
-app.get('/campgrounds/:id', async(req, res) => {
+app.get('/campgrounds/:id', async (req, res) => {
     const campground = await (Campground.findById(req.params.id));
-    res.render('campgrounds/show', {campground});
+    res.render('campgrounds/show', { campground });
 })
 
 // EDIT CAMPGROUND DETAILS
-app.get('/campgrounds/:id/edit', async(req, res) => {
+app.get('/campgrounds/:id/edit', async (req, res) => {
     const campground = await (Campground.findById(req.params.id));
-    res.render('campgrounds/edit', {campground});
+    res.render('campgrounds/edit', { campground });
 })
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
     console.log('LISTENING ON PORT 3000 SAH!!!')
 })
 
 // EDIT logic, what happends when you press "Update Campground" button
-app.put('/campgrounds/:id', async(req, res) => {
+app.put('/campgrounds/:id', async (req, res) => {
     // destruct request to pull id
-    const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
 // DELETE campground
-app.delete('/campgrounds/:id', async(req, res)=> {
-    const {id} = req.params;
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+})
+
+// BASIC ERROR HANDLER
+app.use((err, req, res, next) => {
+    res.send("OOPSIE something went wrong o_o")
 })
