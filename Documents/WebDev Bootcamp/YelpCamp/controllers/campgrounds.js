ce3eb0bcaf778ac8ac16b7574528561198a616f3
Saif Uddin <saifA.uddiN@gmail.com>
2024-07-14 23:04:43 -0700

    added cloudinary image dfeletion to image deletion

--- Documents/WebDev Bootcamp/YelpCamp/controllers/campgrounds.js
+++ Documents/WebDev Bootcamp/YelpCamp/controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require('../models/campground');
+const { cloudinary } = require("../cloudinary")
 
 module.exports.index = async (req, res) => {
     const campgrounds = await (Campground.find({}));
@@ -63,6 +64,9 @@ module.exports.updateCampground = async (req, res) => {
     campground.images.push(...imgs);
     await campground.save();
     if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
         // pulls any image that was added to our deleteImages[] array
         await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
         console.log(campground);
