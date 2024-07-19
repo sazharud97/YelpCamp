e4faa74263f5ef2ce9f5d4aa82031b9b98b4196d
Saif Uddin <saifA.uddiN@gmail.com>
2024-07-19 13:34:41 -0700

    final commit till after react, working state. Removed maptiler dependency from model and set default rating to 5 stars

--- Documents/WebDev Bootcamp/YelpCamp/models/campground.js
+++ Documents/WebDev Bootcamp/YelpCamp/models/campground.js
@@ -21,17 +21,17 @@ const CampgroundSchema = new Schema({
     price: String,
     description: String,
     location: String,
-    geometry: {
-        type: {
-            type: String,
-            enum: ['Point'],
-            required: true,
-        },
-        coordinates: {
-            type: [Number],
-            required: true
-        }
-    },
+    // geometry: {
+    //     type: {
+    //         type: String,
+    //         enum: ['Point'],
+    //         required: true,
+    //     },
+    //     coordinates: {
+    //         type: [Number],
+    //         required: true
+    //     }
+    // },
     author: {
         type: Schema.Types.ObjectId,
         ref: 'User'
